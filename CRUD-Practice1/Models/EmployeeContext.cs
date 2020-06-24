6d5b41739bded76314c35111366108627b4cebd9
Alif <alaminalifaa7@gmail.com>
2020-06-24 12:57:24 +0600

    Simple CRUD

---
+++ CRUD-Practice1/Models/EmployeeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Practice1.Models
+{
+    public class EmployeeContext: DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CRUD-Practice1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_Practice1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
